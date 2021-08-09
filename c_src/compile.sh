ac6ab6a4b5792f726458645d4f31ed3dc63aa038
Tyler Gordon <tagordon@uw.edu>
2021-08-09 16:33:16 -0700

    tested fortran implementation of solution -- it works

--- c_src/compile.sh
+++ c_src/compile.sh
@@ -1,4 +1,5 @@
 gfortran -c ellip.f90
+gfortran -c phot.f90
 gfortran -c bulirsch.f90
 gcc -O2 -c -Wall -Werror -fpic photlib.c
-gfortran ellip.o bulirsch.o photlib.o -o cwrapper.so
+gfortran ellip.o phot.o bulirsch.o photlib.o -o cwrapper.so
