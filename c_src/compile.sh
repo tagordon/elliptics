745fc2290f99fe8667d99e9e585987ef60ab283f
Tyler Gordon <tagordon@uw.edu>
2021-09-14 12:45:21 -0700

    gfortran optimization flags really help

--- c_src/compile.sh
+++ c_src/compile.sh
@@ -1,6 +1,6 @@
 rm *.o
 rm *.mod
-gfortran -c ellip.f90
-gfortran -c phot.f90
-gcc -O2 -c -Wall -Werror -fpic photlib.c
-gfortran ellip.o phot.o photlib.o -o cwrapper.so
+gfortran -O3 -c ellip.f90
+gfortran -O3 -c phot.f90
+gcc -O3 -c -Wall -Werror -fpic photlib.c
+gfortran -O3 ellip.o phot.o photlib.o -o cwrapper.so
